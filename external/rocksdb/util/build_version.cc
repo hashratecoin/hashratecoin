b443169709dc97ff039694737b6a9d60a606bd08
hashratecoin <hashratecoin@gmail.com>
2019-02-13 00:33:49 -0500

    rocksdb folder fix

---
+++ external/rocksdb/util/build_version.cc
@@ -0,0 +1,4 @@
+#include "build_version.h"
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:969a21191a4f9daa74e84eb60598d8f002470a6c";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:2019-01-28";
+const char* rocksdb_build_compile_date = __DATE__;
